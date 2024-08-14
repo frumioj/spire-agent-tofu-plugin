21d20a9981b261b478f462d9d13acfa82c96ad61
John K <stable.pseudonym@gmail.com>
2024-08-14 12:24:45 -0400

    correcting naming errors

--- cmd/server/tofupop/tofupop_server.go
+++ cmd/server/tofupop/tofupop_server.go
@@ -3,13 +3,13 @@ package main
 import (
 	"github.com/spiffe/spire-plugin-sdk/pluginmain"
 	configv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/service/common/config/v1"
-	nodeattestorv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/plugin/agent/nodeattestor/v1"
+	nodeattestorv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/plugin/server/nodeattestor/v1"
 
 	"github.com/frumioj/spire-agent-tofu-plugin/pkg/server"
 )
 
 func main() {
-	p := agent.New()
+	p := server.New()
 	pluginmain.Serve(
 		nodeattestorv1.NodeAttestorPluginServer(p),
 		configv1.ConfigServiceServer(p),
